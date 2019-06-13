9d278e1d550e1ad74275bb9b54050d7041f668f7
nprofilovic <nprofilovic@gmail.com>
2019-06-13 13:35:37 +0200

    Update and insert new files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import  '../resources/styles.css';
+import Header from '../components/header_footer/Header';
 
 
 class App extends Component {
     render() {
         return (
-            <div>
-                App
+            <div className="App">
+                <Header />
             </div>
         )
     }
