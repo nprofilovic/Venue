c93e0ecda95648493dce91a44dfd2ce4aafd3bdf
nprofilovic <nprofilovic@gmail.com>
2019-06-18 12:59:50 +0200

    Make transparen header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -7,7 +7,19 @@ import SideDrawer from './SideDrawer';
 
 class Header extends Component {
     state = {
-        drawerOpen: false 
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    handleScroll = () => {
+        if(window.scrollY > 0){
+            this.setState({headerShow: true})
+        }else {
+            this.setState({headerShow: false})
+        }
     }
 
     toggleDrawer = (value) => {
@@ -19,7 +31,7 @@ class Header extends Component {
             
             <AppBar
                 position="fixed"
-                style={{backgroundColor: '#2f2f2f', boxShadow: 'none', padding: '10px 0px'}}
+                style={{backgroundColor: this.state.headerShow ? '#2f2f2f': 'transparent', boxShadow: 'none', padding: '10px 0px'}}
             >
               <Toolbar>
                     <div className="header_logo">
