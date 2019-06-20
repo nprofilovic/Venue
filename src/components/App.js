85941b87c562a48c318a0e4330025e41454d5033
nprofilovic <nprofilovic@gmail.com>
2019-06-20 11:02:50 +0200

    Make active menu and scrolling plugin

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import  '../resources/styles.css';
+import {Element} from 'react-scroll';
 import Header from './header_footer/Header';
 import Featured from './featured';
 import VenueInfo from './venueInfo';
@@ -12,13 +13,31 @@ class App extends Component {
     render() {
         return (
             <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+                
                 <Header />
-                <Featured />
-                <VenueInfo />
-                <Highlights />
-                <Pricing />
-                <Location />
+                
+                <Element name="featured">
+                    <Featured />
+                </Element>    
+                
+                <Element name="venueInfo">
+                    <VenueInfo />
+                </Element>
+                
+                <Element name="hightlights">
+                    <Highlights />
+                </Element>
+                
+                <Element name="pricing">
+                    <Pricing />
+                </Element>
+
+                <Element name="location">
+                    <Location />
+                </Element>
+            
                 <Footer />
+
             </div>
         )
     }
--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import {scroller} from 'react-scroll';
+
 
 
 const SideDrawer = (props) => {
+
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element, {
+            duration: 1500,
+            delay: 100,
+            smooth: true,
+            offset: -150
+        });
+        props.onClose(false)
+    }
+
     return (
         <Drawer
             anchor="right"
@@ -12,11 +25,11 @@ const SideDrawer = (props) => {
             onClose={() => props.onClose(false)}
         >
             <List component="nav">
-                <ListItem button onClick={() => console.log('Featured')}>Event starts in</ListItem>
-                <ListItem button onClick={() => console.log('Venue NFO')}>Venue NFO</ListItem>
-                <ListItem button onClick={() => console.log('Highlights')}>Highlights</ListItem>
-                <ListItem button onClick={() => console.log('Pricing')}>Pricing</ListItem>
-                <ListItem button onClick={() => console.log('Location')}>Location</ListItem>
+                <ListItem button onClick={() => scrollToElement('featured')}>Event starts in</ListItem>
+                <ListItem button onClick={() => scrollToElement('venueInfo')}>Venue NFO</ListItem>
+                <ListItem button onClick={() => scrollToElement('hightlights')}>Highlights</ListItem>
+                <ListItem button onClick={() => scrollToElement('pricing')}>Pricing</ListItem>
+                <ListItem button onClick={() => scrollToElement('location')}>Location</ListItem>
             </List>
         </Drawer>
     );
